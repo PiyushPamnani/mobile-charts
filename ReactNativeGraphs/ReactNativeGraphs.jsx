9842a0150cc432a6ce267dd888b72b3413b8b300
Piyush Pamnani <piyushpamnani46@gmail.com>
2024-06-21 17:49:17 +0530

    Alternate Method for PDF Generation

--- ReactNativeGraphs/ReactNativeGraphs.jsx
+++ ReactNativeGraphs/ReactNativeGraphs.jsx
@@ -1,14 +1,24 @@
 import React, {useEffect, useRef, useState} from 'react';
-import {ScrollView, Button, View, Image, Text} from 'react-native';
+import {
+  ScrollView,
+  Button,
+  View,
+  Text,
+  PermissionsAndroid,
+  Platform,
+  Alert,
+} from 'react-native';
 import {BarChart, LineChart, PieChart} from 'react-native-gifted-charts';
-import RNHTMLtoPDF from 'react-native-html-to-pdf';
 import {captureRef} from 'react-native-view-shot';
 import Navbar from '../Navbar/Navbar';
+import RNFS from 'react-native-fs';
+import {PDFDocument, rgb, PageSizes} from 'pdf-lib';
+import analytics from '@react-native-firebase/analytics';
 
 const sampleData = [1, 2, 3, 4, 5];
 
 const generateData = () => {
-  return sampleData.map((value, index) => ({
+  return sampleData.map(() => ({
     value: Math.random() * 10,
   }));
 };
@@ -21,39 +31,56 @@ const ReactNativeGraphs = ({route}) => {
   const [graphsArr, setGraphsArr] = useState([]);
   const [chartData, setChartData] = useState([]);
   const [chartImages, setChartImages] = useState([]);
-
+  const [loading, setLoading] = useState(false);
+  const chartRefs = useRef([]);
   const number = route?.params?.chartNumber;
-  const chartRefs = useRef(Array.from({length: number}, () => null));
 
   useEffect(() => {
-    const newGraphsArr = [];
-    for (let i = 0; i < parseInt(number, 10); i++) {
-      newGraphsArr.push(i);
-    }
+    const requestPermissions = async () => {
+      if (Platform.OS === 'android') {
+        try {
+          const granted = await PermissionsAndroid.requestMultiple([
+            PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
+            PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE,
+          ]);
+          if (
+            granted[PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE] ===
+              PermissionsAndroid.RESULTS.GRANTED &&
+            granted[PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE] ===
+              PermissionsAndroid.RESULTS.GRANTED
+          ) {
+            console.log('You can use the storage');
+          } else {
+            console.log('Storage permission denied');
+          }
+        } catch (err) {
+          console.warn(err);
+        }
+      }
+    };
+
+    requestPermissions();
+  }, []);
+
+  useEffect(() => {
+    const newGraphsArr = Array.from(
+      {length: parseInt(number, 10)},
+      (_, i) => i,
+    );
     setGraphsArr(newGraphsArr);
   }, [number]);
 
   useEffect(() => {
-    const data = [];
-    for (let i = 0; i < graphsArr.length; i++) {
-      data.push({
-        type: chartTypes[i % chartTypes.length],
-        data: generateData(),
-      });
-    }
+    const data = graphsArr.map((_, i) => ({
+      type: chartTypes[i % chartTypes.length],
+      data: generateData(),
+    }));
     setChartData(data);
   }, [graphsArr]);
 
   const handleScroll = event => {
     const currentScrollOffset = event.nativeEvent.contentOffset.y;
-    const scrollUp = currentScrollOffset < lastScrollOffset;
-
-    if (scrollUp) {
-      setNavbarVisible(true);
-    } else {
-      setNavbarVisible(false);
-    }
-
+    setNavbarVisible(currentScrollOffset < lastScrollOffset);
     setLastScrollOffset(currentScrollOffset);
   };
 
@@ -63,98 +90,78 @@ const ReactNativeGraphs = ({route}) => {
         format: 'png',
         quality: 0.8,
       });
-      console.log(chartContainerRef, '****');
-      setChartImages(prevImages => [...prevImages, chartContainerRef]);
+      setChartImages(prevImages => {
+        const newImages = [...prevImages];
+        newImages[index] = chartContainerRef;
+        return newImages;
+      });
     }
   };
 
   const generatePdf = async () => {
-    const pdfPages = [];
-    const chartsPerPage = 4;
+    setLoading(true);
+    try {
+      const pdfDoc = await PDFDocument.create();
+      const chartsPerRow = 2;
+      const chartsPerPage = 4;
+      const margin = 50;
+      const imageWidth = 200;
+      const imageHeight = 200;
+      const xOffset =
+        (PageSizes.A4[0] -
+          chartsPerRow * imageWidth -
+          (chartsPerRow - 1) * margin) /
+        2;
+      const yOffset = PageSizes.A4[1] - 100;
 
-    for (let i = 0; i < graphsArr.length; i += chartsPerPage) {
-      const pageCharts = graphsArr.slice(i, i + chartsPerPage);
+      for (let i = 0; i < graphsArr.length; i += chartsPerPage) {
+        let page = pdfDoc.addPage(PageSizes.A4);
+        page.drawText(`Report Date: ${new Date().toLocaleDateString()}`, {
+          x: margin,
+          y: PageSizes.A4[1] - margin,
+          size: 20,
+          color: rgb(0, 0, 0),
+        });
 
-      const headerHtml = `
-          <div class="header">
-            <div>Report Date: ${new Date().toLocaleDateString()}</div>
-          </div>
-        `;
+        const pageCharts = graphsArr.slice(i, i + chartsPerPage);
+        for (let j = 0; j < pageCharts.length; j++) {
+          const chartIndex = pageCharts[j];
+          if (chartImages[chartIndex]) {
+            const pngImageBytes = await RNFS.readFile(
+              chartImages[chartIndex],
+              'base64',
+            );
+            const pngImage = await pdfDoc.embedPng(pngImageBytes);
 
-      const footerHtml = `
-          <div class="footer">
-            <a href="https://example.com">Click here for more information</a>
-          </div>
-        `;
+            const xPosition =
+              xOffset + (j % chartsPerRow) * (imageWidth + margin);
+            const yPosition =
+              yOffset -
+              Math.floor(j / chartsPerRow) * (imageHeight + margin) -
+              200;
 
-      let pageHtml = `
-          <html>
-            <head>
-              <style>
-                body {
-                  font-family: Arial, sans-serif;
-                  margin: 0;
-                  padding: 0;
-                }
-                .container {
-                  display: grid;
-                  grid-template-columns: 1fr 1fr;
-                  padding: 20px;
-                }
-                .chart-container {
-                  width: 100%;
-                }
-                .chart-img {
-                  width: 100%;
-                  height: auto;
-                  margin-bottom: 20px;
-                }
-                .header, .footer {
-                  width: 100%;
-                  text-align: center;
-                }
-                .footer a {
-                  color: blue;
-                  text-decoration: none;
-                }
-              </style>
-            </head>
-            <body>
-              ${headerHtml}
-              <div class="container">
-        `;
-
-      for (let j = 0; j < pageCharts.length; j++) {
-        const chartIndex = pageCharts[j];
-        if (chartImages[chartIndex]) {
-          console.log(chartImages[chartIndex]);
-          pageHtml += `
-              <div class="chart-container">
-                <p>Chart ${chartIndex + 1}</p>
-                <img src="${chartImages[chartIndex]}" class="chart-img" />
-              </div>
-            `;
+            page.drawImage(pngImage, {
+              x: xPosition,
+              y: yPosition,
+              width: imageWidth,
+              height: imageHeight,
+            });
+          }
         }
       }
 
-      pageHtml += `
-              </div>
-              ${footerHtml}
-            </body>
-          </html>
-        `;
-
-      pdfPages.push(pageHtml);
+      const pdfBytes = await pdfDoc.saveAsBase64();
+      const pdfPath = '/storage/emulated/0/Download/graphs.pdf';
+      await RNFS.writeFile(pdfPath, pdfBytes, 'base64');
+      await analytics().logEvent('pdf_generated', {
+        pdfPath,
+      });
+      Alert.alert(`PDF saved at: ${pdfPath}`);
+    } catch (error) {
+      console.error('Error generating PDF:', error);
+      Alert.alert('Error', 'Failed to generate PDF. Please try again.');
     }
-
-    const options = {
-      html: pdfPages.join('<div style="page-break-after:always;"></div>'),
-      fileName: 'mobile-charts',
-      directory: 'Documents',
-    };
-
-    const pdf = await RNHTMLtoPDF.convert(options);
-    console.log(pdf.filePath);
+    setLoading(false);
   };
 
   return (
@@ -165,7 +172,8 @@ const ReactNativeGraphs = ({route}) => {
             key={n}
             collapsable={false}
             ref={ref => (chartRefs.current[index] = ref)}
-            onLayout={() => captureChartImage(index)}>
+            onLayout={() => captureChartImage(index)}
+            style={{marginBottom: 20}}>
             <Text>Chart {n + 1}</Text>
             {chartData[index]?.type === 'line' && (
               <LineChart data={chartData[index]?.data} />
@@ -181,7 +189,11 @@ const ReactNativeGraphs = ({route}) => {
             )}
           </View>
         ))}
-        <Button title="Generate PDF" onPress={generatePdf} />
+        <Button
+          title={loading ? 'Preparing PDF' : 'Download PDF'}
+          onPress={generatePdf}
+          disabled={loading}
+        />
       </ScrollView>
       <Navbar navbarVisible={navbarVisible} />
     </View>
